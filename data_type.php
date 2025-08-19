d2af627fe0334a7b61f1e66846b929931d8ff672
Andi Salam Syahputra <andiku0755@gmail.com>
2025-08-19 16:11:35 +0700

    add static string

--- data_type.php
+++ data_type.php
@@ -2,13 +2,15 @@
 
 $umur = 23; //int
 $jarak = 19.99; // float
-$nama = "Andi"; // String
+$firstNama = "Andi"; // String non statis
+$lastnama = 'Salam Syahputra'; // String statis
 $isHandsome = true; // Boolean
 $hoby = ["Swim", "Hunting", "Programming"];
 
 //var_dump($umur, $jarak, $nama, $isHandsome);
 print_r($umur);
 print_r($jarak);
-print_r($nama);
+print_r($firstNama);
+print_r($lastnama);
 print_r($isHandsome);
 print_r($hoby);
