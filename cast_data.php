ca8396843a44c33939ffcc6cd40ca54761c8a5ae
Andi Salam Syahputra <andiku0755@gmail.com>
2025-08-19 16:19:43 +0700

    add many variable and variants data type

--- cast_data.php
+++ cast_data.php
@@ -2,10 +2,14 @@
 
 $jarak = '25 kilometer';
 $nilai = '25.31';
+$nilai2 = 2;
+$nilai3 = true;
 
 // do cast
 
 $jarak = (float) $jarak;
 $nilai = (float) $nilai;
+$nilai2 = (string) $nilai2;
+$nilai3 = (integer) $nilai3;
 
-var_dump($jarak, $nilai);
+var_dump($jarak, $nilai, $nilai2, $nilai3);
