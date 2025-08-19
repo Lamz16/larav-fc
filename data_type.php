0354adce52c0a2e2a02420765b70896b41938c84
Andi Salam Syahputra <andiku0755@gmail.com>
2025-08-19 16:07:08 +0700

    data type chapter

---
+++ data_type.php
@@ -0,0 +1,12 @@
+<?php
+
+$umur = 23; //int
+$jarak = 19.99; // float
+$nama = "Andi"; // String
+$isHandsome = true; // Boolean
+
+var_dump($umur, $jarak, $nama, $isHandsome);
+print_r($umur);
+print_r($jarak);
+print_r($nama);
+print_r($isHandsome);
