0d5a14b0b512bcfe589e59d9cd9959f1b4f7a73c
Andi Salam Syahputra <andiku0755@gmail.com>
2025-08-19 16:09:14 +0700

    add array

--- data_type.php
+++ data_type.php
@@ -4,9 +4,11 @@ $umur = 23; //int
 $jarak = 19.99; // float
 $nama = "Andi"; // String
 $isHandsome = true; // Boolean
+$hoby = ["Swim", "Hunting", "Programming"];
 
-var_dump($umur, $jarak, $nama, $isHandsome);
+//var_dump($umur, $jarak, $nama, $isHandsome);
 print_r($umur);
 print_r($jarak);
 print_r($nama);
 print_r($isHandsome);
+print_r($hoby);
