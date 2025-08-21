672be3ee318abd7f8a47fc9282c4f6f7d93ddb83
Andi Salam Syahputra <andiku0755@gmail.com>
2025-08-21 14:02:44 +0700

    add new route

---
+++ resources/views/home.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+</head>
+<body>
+    <h1>
+        Hello Everyone
+    </h1>
+    <ul>
+        <li><h4>I am Andi Salam Syahputra</h4></li>
+        <li><h4>My Profession is Android Developer</h4></li>
+        <li><h4>I have many hobbies like swimming, hunting, hiking, and playing video games</h4></li>
+    </ul>
+
+    <table border="3" cellpadding="10">
+        <thead>
+            <tr>
+                <th>Hobby</th>
+                <th>Skill Level</th>
+                <th>Years Practiced</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Swimming</td>
+                <td>Intermediate</td>
+                <td>5</td>
+            </tr>
+            <tr>
+                <td>Hunting</td>
+                <td>Beginner</td>
+                <td>2</td>
+            </tr>
+            <tr>
+                <td>Hiking</td>
+                <td>Advanced</td>
+                <td>7</td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
