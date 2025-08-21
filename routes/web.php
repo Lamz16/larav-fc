e6e6a63ffd32f1232ad1f2a54336fecf6cb20510
Andi Salam Syahputra <andiku0755@gmail.com>
2025-08-21 14:37:27 +0700

    add new routes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', function(){
+Route::get('/home', function () {
     return view('home');
+});
+
+Route::prefix('buku')->group(function () {
+
+    Route::get('/', [BookController::class, 'index']);
+
+    Route::get('/detail/{id?}', [BookController::class, 'show']);
 });
