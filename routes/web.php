544fb8feb3e26f127748c5531b21f5a95c93cb57
Andi Salam Syahputra <andiku0755@gmail.com>
2025-08-22 10:00:26 +0700

    add barang model ~ Migration and model chapter

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController as BC;
+use App\Models\Barang as Brg;
+use App\Models\User as Usr;
 
 Route::get('/', function () {
     return view('welcome');
@@ -23,4 +25,37 @@ Route::prefix('buku')->group(function () {
     Route::get('/', [BC::class, 'index']);
 
     Route::get('/detail/{id?}', [BC::class, 'show']);
+});
+
+Route::get('/testing', function(){
+    // $user = new Usr;
+    // $user ->name ='Andi Salam Syahputra';
+    // $user ->email ='andiku0755@gmail.com';
+    // $user ->password = bcrypt('password');
+    // $user -> save();
+
+    // $user = Usr::create([
+    //     'name' => 'Hapidin Azis',
+    //     'email'=> 'hapiding@gmail.com',
+    //     'password' => bcrypt('password'),
+    // ]);
+
+    // $users = Usr::all();
+    // dd($users);
+
+    // Brg::create([
+    //     'users_id' => 1,
+    //     'name' => 'Buku 1',
+    //     'description' => 'Buku ke 1'
+    // ]);
+
+    $user = Usr::find(1);
+    // $user->name = 'Salam';
+    // $user->save();
+
+    $user -> update([
+        'name' => 'Andi Salam Syahputra',
+        'email' => 'andiku07@gmail.com',
+        'password' => 'pw',
+    ]);
 });
