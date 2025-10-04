387c72bf7252c23eb23301ff953452a6463169ac
Lamz16 <andiku0755@gmail.com>
2025-10-04 15:11:19 +0700

    update format

--- app/Helper.php
+++ app/Helper.php
@@ -4,6 +4,6 @@
 if(!function_exists('formatRupiah')){
 
     function formatRupiah(float $number){
-        return 'Rp.' . number_format($number);
+        return 'Rp. ' . number_format($number);
     }
 }
